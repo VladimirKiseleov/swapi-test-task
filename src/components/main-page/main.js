26646afb880e2bb64202f97ba2b48e72b0038565
Vladimir <kiselev7772@yandex.ru>
2021-05-01 15:20:59 +0400

    i deps, add pages

---
+++ src/components/main-page/main.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class main extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+main.propTypes = {
+
+};
+
+export default main;
