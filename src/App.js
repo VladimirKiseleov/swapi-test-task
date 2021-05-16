0e956f3d3ab08215b44571ea2375eaa844fc3dd9
Vladimir <kiselev7772@yandex.ru>
2021-05-16 14:44:43 +0400

    improved styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Navbar } from './components/Navbar'
 import { Home } from './pages/Home'
 import { Films } from './pages/Films'
 import { People } from './pages/People'
@@ -9,23 +8,62 @@ import { Species } from './pages/Species'
 import { Starships } from './pages/Starships'
 import { Vehicles } from './pages/Vehicles'
 import './styles/styles.css'
+import 'antd/dist/antd.css'
+import { Layout, Menu } from 'antd'
+import { NavLink } from 'react-router-dom'
+
+const { Header, Content, Footer } = Layout
 
 // можно добавить пагинацию
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <div className="center">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/films" component={Films} />
-          <Route path="/people" component={People} />
-          <Route path="/planets" component={Planets} />
-          <Route path="/species" component={Species} />
-          <Route path="/starships" component={Starships} />
-          <Route path="/vehicles" component={Vehicles} />
-        </Switch>
-      </div>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">
+              <NavLink exact to="/">
+                Home
+              </NavLink>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <NavLink to="/films">Films</NavLink>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <NavLink to="/people">People</NavLink>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <NavLink to="/planets">Planets</NavLink>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <NavLink to="/species">Species</NavLink>
+            </Menu.Item>
+            <Menu.Item key="6">
+              <NavLink to="/starships">Starships</NavLink>
+            </Menu.Item>
+            <Menu.Item key="7">
+              <NavLink to="/vehicles">Vehicles</NavLink>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div className="site-layout-content">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/films" component={Films} />
+              <Route path="/people" component={People} />
+              <Route path="/planets" component={Planets} />
+              <Route path="/species" component={Species} />
+              <Route path="/starships" component={Starships} />
+              <Route path="/vehicles" component={Vehicles} />
+            </Switch>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          SWAPI-test-task by Vladimir Kiseleov
+        </Footer>
+      </Layout>
     </BrowserRouter>
   )
 }
